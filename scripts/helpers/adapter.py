08dd29061a687b0f16be8ec0f10a0580f30f45bd
ahmetustun <ahmetustun89@gmail.com>
2022-05-30 14:40:42 +0200

    +bugfix

--- scripts/helpers/adapter.py
+++ scripts/helpers/adapter.py
@@ -16,7 +16,7 @@ class Adapter(nn.Module):
 
         self.down = nn.Linear(self.input_dim, self.adapter_dim)
         self.up = nn.Linear(self.adapter_dim, self.input_dim)
-        self.layernorm = nn.LayerNorm(config.input_dim)
+        self.layernorm = nn.LayerNorm(self.input_dim)
 
         self.init_linear_layer(self.down, std=self.initializer_range)
         self.init_linear_layer(self.up, std=self.initializer_range)
